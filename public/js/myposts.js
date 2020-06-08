dd5493f580821942c36b247dd87fae276b742bd4
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-06-08 15:55:49 -0400

    My posts html and js built out

---
+++ public/js/myposts.js
@@ -0,0 +1,45 @@
+var updateForm = $("#updateForm");
+var updatedPost = $("#updatedPost");
+var file = $("#file");
+var hashtag = $("#hashtag");
+var deletePost = $("#deletePost");
+var user = localStorage.getItem('user');
+var postAlert = $("#postAlert");
+
+deletePost.on("click",function(){
+    $.delete(`/api/posts/${user}`)
+    .then(function() {
+      renderPosts();
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
+
+updateForm.on("submit",function(event){
+    if (newPost.val().trim() == "" ){
+        postAlert.removeAttr("hidden")
+        postAlert.text("Please fill out Post field")
+        return;
+    } 
+
+    var post = {
+        author: user,
+        text: loginPassword.val().trim(),
+        image: file[0].files[0],
+        timestamp: moment().format()
+    }
+
+    $.ajax({
+        method: "PUT",
+        url: "/api/posts",
+        data: post
+      })
+        .then(function() {
+            renderPosts();
+        });
+});
+
+function renderPosts(){
+
+}
