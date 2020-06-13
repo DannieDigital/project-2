7a9af56fe82d04626c03ae0300c9d03a793242bd
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-06-13 00:37:21 -0400

    Index rewritten to use local storage to store images. Still issue with login function

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ var signupEmail = $("#email");
 var signupPassword = $("#signupPassword");
 var loginAlert = $("#loginAlert");
 var signupAlert = $("#signupAlert")
-
+var profilePic = $("#blah");
 
 login.on("submit", function(event){
 
@@ -28,11 +28,40 @@ login.on("submit", function(event){
     loginUser(username, password);
 });
 
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+
+      $('#blah')
+        .attr('src', e.target.result)
+        
+    };
+    reader.readAsDataURL(input.files[0] );
+  }
+}
+
+function getBase64Image(img) {
+  var canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+
+  var dataURL = canvas.toDataURL("image/png");
+
+  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
 signUp.on("submit", function(event){
     event.preventDefault(event);
+ 
+    imgData = getBase64Image(blah);
+    localStorage.setItem("avatar", imgData);
 
     var userProfile = {
-        avatar: file[0].files[0],
+        // avatar: file[0].files[0],
         firstname: firstName.val().trim(),
         lastname: lastName.val().trim(),
         username: signupUsername.val().trim(),
@@ -40,7 +69,12 @@ signUp.on("submit", function(event){
         password: signupPassword.val().trim()
     }
 
-    console.log(userProfile);
+    firstName.val("");
+    lastName.val("");
+    signupUsername.val("");
+    signupEmail.val("");
+    signupPassword.val("");
+    // console.log(userProfile);
     
     if (!userProfile.firstname || !userProfile.lastname || !userProfile.username || !userProfile.email || !userProfile.password){
         signupAlert.removeAttr("hidden")
@@ -54,7 +88,7 @@ signUp.on("submit", function(event){
       console.log(data)
     })
         .then(function() {
-          // loginUser(userProfile.userName, userProfile.password)
+          loginUser(userProfile.username, userProfile.password);
         })
         .catch(function(err) {
           console.log(err);
@@ -62,28 +96,53 @@ signUp.on("submit", function(event){
 })
 
 
-// We still need a login api something that takes the user's username and password and queries the database to see if it's a valid login
 
 function loginUser(username, password){
-    // Needs to be tested
-    $.post("/api/login", {
+
+  console.log(username);
+  $.get(`/api/users`, function(data){
+    data.forEach(user => {
+      if (user.username == username){
+        localStorage.setItem('userId',user.id);
+      }
+      
+    })
+
+  })
+    // Not working
+    $.post("/login", {
         username: username,
         password: password
       })
         .then(function() {
-          window.location.replace(`/myprofile/${username}`);
+          window.location.replace(`/mytimeline.html`);
           // If there's an error, log the error
         })
         .catch(function(err) {
           console.log(err);
         });
 
-        localStorage.setItem('user', username);
-
-    // Maybe a get to get the users id and save it to local storage
-
-    // $.get(`/api/users/${username}`, function(data){
-
-    // })
-
 }
+
+
+// }
+
+var localStorageSpace = function(){
+  var data = '';
+
+  console.log('Current local storage: ');
+
+  for(var key in window.localStorage){
+
+      if(window.localStorage.hasOwnProperty(key)){
+          data += window.localStorage[key];
+          console.log( key + " = " + ((window.localStorage[key].length * 16)/(8 * 1024)).toFixed(2) + ' KB' );
+      }
+
+  }
+
+  console.log(data ? '\n' + 'Total space used: ' + ((data.length * 16)/(8 * 1024)).toFixed(2) + ' KB' : 'Empty (0 KB)');
+  console.log(data ? 'Approx. space remaining: ' + (5120 - ((data.length * 16)/(8 * 1024)).toFixed(2)) + ' KB' : '5 MB');
+};
+
+localStorageSpace();
