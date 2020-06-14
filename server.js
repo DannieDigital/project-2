f18aa4a350d38feee26284dd42241e6a1716e61d
Danielle Beaty <daniellerbeaty@gmail.com>
2020-06-14 01:30:07 -0400

    trying to fix deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var serveStatic = require('serve-static')
 var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
