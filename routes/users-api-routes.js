840b666c6ec421442fbcea2ac50726c5d7b716ad
clemson tigress <anreeves83@gmail.com>
2020-06-14 16:55:41 -0400

    Follows model and followers routes working

--- routes/users-api-routes.js
+++ routes/users-api-routes.js
@@ -23,14 +23,44 @@ module.exports = function(app) {
         res.json(dbUsers);
       });
     });
+
+    app.get("/api/followers", function(req, res) {
+        //this route works
+      //gets all users as "followingId" plus all their followers
+      db.Follows.findAll({
+        include: ["follower"]
+      }).then(function(dbFollows) {
+        res.json(dbFollows);
+      });
+    });
+
+    /*app.get("/api/users/followers/:id", function(req, res) {
+      //gets user by id and includes all their followers
+      db.Users.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [db.Followers]
+      }).then(function(dbUsers) {
+        res.json(dbUsers);
+      });
+    });*/
   
     //this route works
     app.post("/api/users", function(req, res) {
       db.Users.create(req.body).then(function(dbUsers) {
         res.json(dbUsers);
       });
+      
     });
-  
+
+    //this route works
+    app.post("/api/followers", function(req, res) {
+        db.Follows.create(req.body).then(function(dbFollows) {
+          res.json(dbFollows);
+        });
+        
+      });
     
   
   };
