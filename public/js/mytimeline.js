ed7ff9f6fa9db558d8c0f7bf3c82577bec822a41
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-06-14 20:01:24 -0400

    index and mytimeline done

--- public/js/mytimeline.js
+++ public/js/mytimeline.js
@@ -4,54 +4,85 @@ var file = $("#file");
 var hashtag = $("#hashtag");
 var postAlert = $("#postAlert");
 var userId = localStorage.getItem('userId');
-var stockPhoto = "assets/profile.png";
+// var stockPhoto = "assets/profile.png";
 var allPosts = $("#posts");
-var dataImage = localStorage.getItem('avatar');
+// var dataImage = localStorage.getItem('avatar');
 var hashtagArr = [];
+var fileLoc = $("#fileLoc");
 
 renderPosts();
 
 // Get all the hashtags in the database and save to an array 
-$.get(`/api/hastags`, function(data){
-  console.log(data);
 
-  data.forEach(hashtag => {
-    hashtagArr.push({
-      id: hashtag.id,
-      hashtag: hashtag.tagname
-
-    });
-    })
-  });
 
 postForm.on("submit", function(event){
 
-    event.preventDefault(event);
+  event.preventDefault(event);
+
+  $.get(`/api/hashtags`, function(data){
+    console.log(data);
+  
+    data.forEach(hashtag => {
+      hashtagArr.push({
+        id: hashtag.id,
+        hashtag: hashtag.tagname
+  
+      });
+    })
+  }).then(function(){
     if (newPost.val().trim() == "" ){
-        postAlert.removeAttr("hidden")
-        postAlert.text("Please fill out Post field")
-        return;
+      postAlert.removeAttr("hidden")
+      postAlert.text("Please fill out Post field")
+      return;
+    }
+
+    imgData = getBase64Image(blah);
+    var image;
+
+    if (file[0].files[0]){
+      image = file[0].files[0].name;
+      localStorage.setItem(`${image}`, imgData);
+    }else if (!file[0].files[0]){
+      image = fileLoc.val().trim()
+    }
+    
+    if(!image){
+      image = 0;
     }
 
     // Get form data 
     var post = {
-        UserId: userId,
-        text: newPost.val().trim(),
-        HashtagId: null,
-        hashtag: hashtag.val().trim()
-        // image: file[0].files[0] || null,
+      UserId: userId,
+      text: newPost.val().trim(),
+      HashtagId: null,
+      hashtag: hashtag.val().trim(),
+      image: image,
     }
 
-    hashtagArr.forEach(hashtag => {
+    newPost.val("");
+    hashtag.val("");
+    file.val("");
+    fileLoc.val("");
+    $("#blah") .attr('src', "assets/1x1.png");
 
-      if (post.hashtag == hashtag.tagline){
-        post.HashtagId == hashtag.id;
+    hashtagArr.forEach(hashtag => {
+      // console.log(`Post.hashtag: ${post.hashtag} Hashtag.hashtag: ${hashtag.hashtag}`)
+      if (post.hashtag == hashtag.hashtag){
+        post.HashtagId = hashtag.id;
+        // console.log("they are equal!");
+
       }
 
     });
 
+    // console.log(hashtagArr);
+    // console.log(post.HashtagId)
+
     if(!post.HashtagId){
-      $.post("/api/hashtags", {tagname: post.hashtag})
+      $.post("/api/hashtags", {tagname: post.hashtag}, function(data){
+
+        post.HashtagId = data.id;
+      })
         .then(function() {
           $.post("/api/posts", post)
             .then(function() {
@@ -74,15 +105,28 @@ postForm.on("submit", function(event){
           console.log(err);
         });
     }
+
+    });
+    
+    
 });
 
 function makePost(){
 
-  $.get(`/api/users/1`, function(data){
+  $.get(`/api/users/${userId}`, function(data){
     console.log(data);
 
+    var image = localStorage.getItem(`${data.avatar}`);
+    var photo;
+
+    if (image){
+      photo = "data:image/png;base64," + image;
+    }else{
+      photo = data.avatar;
+    }
+
     var rowPost = $(`<div class = "row post"></div>`)
-    var profilePic = $(`<div class = "col-1"><img src="${"data:image/png;base64," + dataImage}" width="50" height="50"></div>`)
+    var profilePic = $(`<div class = "col-1"><img src="${photo}" width="50" height="50"></div>`)
     var newPostBttn = $(`<div class = "col-7 mx-auto">
     <div class="card">
       <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#newPost">New Post</button>
@@ -98,48 +142,67 @@ function makePost(){
 
 }
 
+function getImage(photoData){
+  var image = localStorage.getItem(`${photoData}`);
+  var photo;
+
+  if (image){
+    photo = "data:image/png;base64," + image;
+  }else{
+    photo = photoData;
+  }
+
+  return photo;
+}
 function renderPosts(){
   allPosts.empty();
   makePost();
 // On the back end the orm would return the posts of all the users "user" follows. Thats why we need the user in url. This route could be confused with the one to return all posts from a certain user. Maybe we could name that one "/api/posts/user"
     $.get("/api/posts", function(data){
-        console.log(data);
-        data.forEach(post => {
+        // console.log(data);
+        var tagname = "";
+
+
+        hashtagArr.forEach(hashtag => {
+          if (data.HashtagId == hashtag.id){
+            tagname = hashtag.hashtag; 
+          }  
+        }); 
+
+        console.log(tagname)
+
+          data.forEach(post => {
+
+            
           
-          var postRow = $(`<div class = "row post">`);
-          var profilePic = $(`<div class = "col-1"><img src="${post.User.avatar || stockPhoto}" width="50" height="50"></div>`);
-          var postDiv = $(`<div class = "col-11">`);
-          var postCard = $(`<div class="card">`);
-          var popup = $(`<div class="card-body popup">`);
-          var cardText = $(`<p class="card-text">${post.text}</p>`);
-          var hashtag = $(`<p class="card-text"><small class="text-muted">${post.hashtag || " "}</small></p>`);
-          var postImage = $(`<img src="${post.image}" class="card-img-top" alt="..."width="300" height="200">`);
-          var footer = $(`<div class="card-footer text-muted mx-auto">
-          ${moment(post.updatedAt).fromNow()}</div>`);
+            var postRow = $(`<div class = "row post">`);
+            var profilePic = $(`<div class = "col-1"><img src="${getImage(post.User.avatar)}" width="50" height="50"></div>`);
+            var postDiv = $(`<div class = "col-11">`);
+            var postCard = $(`<div class="card">`);
+            var popup = $(`<div class="card-body popup">`);
+            var cardText = $(`<p class="card-text">${post.text}</p>`);
+            var hashtag = $(`<p class="card-text"><small class="text-muted">${tagname}</small></p>`);
+            var postImage = $(`<img src="${getImage(post.image)}" class="card-img-top" alt="..."width="300" height="200">`);
+            var footer = $(`<div class="card-footer text-muted mx-auto">
+            ${moment(post.updatedAt).fromNow()}</div>`);
 
-          postRow.append(profilePic);
-          popup.append(cardText);
-          popup.append(hashtag);
+            postRow.append(profilePic);
+            popup.append(cardText);
+            popup.append(hashtag);
 
-          if (post.image != 0){
+            if (post.image != 0){
               popup.append(postImage);
-          }
+            }
 
-          popup.append(footer);
+            popup.append(footer);
 
-          postCard.append(popup);
-          postDiv.append(postCard);
-          postRow.append(postDiv);
-          allPosts.append(postRow);
+            postCard.append(popup);
+            postDiv.append(postCard);
+            postRow.append(postDiv);
+            allPosts.append(postRow);
+          });
         });
-
-        
-
-    });
-
-
-  
-
+   
 }
 
 function readURL(input) {
