057d667457302234d209db46afadfb08fabad220
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-06-14 22:29:17 -0400

    My posts html and js done

--- public/js/myposts.js
+++ public/js/myposts.js
@@ -8,6 +8,16 @@ var postAlert = $("#postAlert");
 var allPosts = $("#posts");
 var userId = localStorage.getItem('userId');
 var currPost = 0;
+var searchForm = $("#searchForm");
+var searchTerm = $("#searchTerm");
+
+renderPosts();
+
+searchForm.on("submit", function(event){
+  event.preventDefault(event);
+  localStorage.setItem("search", searchTerm.val().trim());
+  window.location.replace(`/search.html`);
+})
 
 $("body").delegate("#deletePost", "click", function(){
 
@@ -15,8 +25,7 @@ $("body").delegate("#deletePost", "click", function(){
 
   $.ajax({
     method: "DELETE",
-    url: "/api/posts"+postToDelete,
-    data: post
+    url: "/api/posts/"+postToDelete,
   })
     .then(function() {
         renderPosts();
@@ -38,7 +47,7 @@ updateForm.on("submit",function(event){
     var post = {
         UserId: userId,
         id: currPost,
-        text: loginPassword.val().trim(),
+        text: newPost.val().trim()
     }
 
     $.ajax({
@@ -51,20 +60,36 @@ updateForm.on("submit",function(event){
         });
 });
 
+function getImage(photoData){
+  var image = localStorage.getItem(`${photoData}`);
+  var photo;
+
+  if (image){
+    photo = "data:image/png;base64," + image;
+  }else{
+    photo = photoData;
+  }
+
+  return photo;
+
+}
+
 function renderPosts(){
 
+  allPosts.empty();
+  
   $.get(`/api/users/${userId}`, function(data){
     console.log(data);
     userPosts = data.Posts
     userPosts.forEach(post => {
       var postRow = $(`<div class = "row post">`)
-      var profilePic = $(` <div class = "col-1"><img src="${data.avatar}" width="50" height="50"></div>`)
+      var profilePic = $(` <div class = "col-1"><img src="${getImage(data.avatar)}" width="50" height="50"></div>`)
       var postDiv = $(`<div class = "col-11">`);
       var postCard = $(`<div class="card">`);
       var popup = $(`<div class="card-body popup">`);
       var cardText = $(`<p class="card-text">${post.text}</p>`);
-      var hashtag = $(`<p class="card-text"><small class="text-muted">${tagname}</small></p>`);
-      var postImage = $(`<img src="${post.image}" class="card-img-top" alt="..."width="300" height="200">`);
+      var hashtag = $(`<p class="card-text"><small class="text-muted">${post.tagname||""}</small></p>`);
+      var postImage = $(`<img src="${getImage(post.image)}" class="card-img-top" alt="..."width="300" height="200">`);
       var relativeTime = $(`<div class="text-muted mx-auto">${moment(post.updatedAt).fromNow()}</div>`);
       var footer = $(`<div class="card-footer text-muted mx-auto">
         <button class="btn btn-primary" data-toggle="modal" data-target="#updateModal" data-postId=${post.id} id="updateBttn">Update</button>
@@ -89,3 +114,23 @@ function renderPosts(){
     });
   });
 }
+
+var localStorageSpace = function(){
+  var data = '';
+
+  console.log('Current local storage: ');
+
+  for(var key in window.localStorage){
+
+      if(window.localStorage.hasOwnProperty(key)){
+          data += window.localStorage[key];
+          console.log( key + " = " + ((window.localStorage[key].length * 16)/(8 * 1024)).toFixed(2) + ' KB' );
+      }
+
+  }
+
+  console.log(data ? '\n' + 'Total space used: ' + ((data.length * 16)/(8 * 1024)).toFixed(2) + ' KB' : 'Empty (0 KB)');
+  console.log(data ? 'Approx. space remaining: ' + (5120 - ((data.length * 16)/(8 * 1024)).toFixed(2)) + ' KB' : '5 MB');
+};
+
+localStorageSpace();
